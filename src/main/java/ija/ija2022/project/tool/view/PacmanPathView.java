fc4eff895ada22f24d028bc03442ccadd888f826
rediso4ka <swimmer030203@gmail.com>
2023-05-04 22:19:35 +0200

    feat: added view for A*

---
+++ src/main/java/ija/ija2022/project/tool/view/PacmanPathView.java
@@ -0,0 +1,33 @@
+package ija.ija2022.project.tool.view;
+
+import ija.ija2022.project.tool.common.CommonMazeObject;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+
+public class PacmanPathView implements ComponentView {
+    private final CommonMazeObject model;
+    private final FieldView parent;
+
+    public PacmanPathView(FieldView parent, CommonMazeObject m) {
+        this.model = m;
+        this.parent = parent;
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle bounds = this.parent.getBounds();
+        double w = bounds.getWidth();
+        double h = bounds.getHeight();
+        Math.max(h, w);
+        double diameter = Math.min(h, w) - 10.0;
+        double x = (w - diameter) / 2.0;
+        double y = (h - diameter) / 2.0;
+        Ellipse2D.Double ellipse = new Ellipse2D.Double(x, y, diameter, diameter);
+        g2.setColor(Color.blue);
+        g2.fill(ellipse);
+    }
+}
