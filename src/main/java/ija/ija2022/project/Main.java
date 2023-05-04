2a923961cb265e4ddccbecc41e20eaee68f98e6c
NickSettler <moiseevnikita14@gmail.com>
2023-05-04 10:02:05 +0200

    refactor: update main class

--- src/main/java/ija/ija2022/project/Main.java
+++ src/main/java/ija/ija2022/project/Main.java
@@ -1,25 +1,13 @@
 package ija.ija2022.project;
 
-import ija.ija2022.project.events.EVENTS;
-//import ija.ija2022.project.events.EventCallback;
-import ija.ija2022.project.events.EventsSystem;
+import ija.ija2022.project.game.GameController;
 import ija.ija2022.project.game.MazeConfigure;
-import ija.ija2022.project.game.PacmanObject;
 import ija.ija2022.project.settings.GAME_MODE;
-import ija.ija2022.project.tool.MazePresenter;
 import ija.ija2022.project.tool.common.CommonMaze;
-import ija.ija2022.project.tool.common.CommonMazeObject;
-import ija.ija2022.project.tool.common.CommonField;
-import ija.ija2022.project.tool.common.Observable;
-import ija.ija2022.project.ui.*;
 
-import java.awt.event.*;
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,8 +15,8 @@ public class Main {
     public static void main(String[] args) {
 //        UIController.getInstance().showMainView();
 
-        Window mainWindow = new Window();
-        mainWindow.getFrame().setVisible(true);
+//        Window mainWindow = new Window();
+//        mainWindow.getFrame().setVisible(true);
 
 //        keyboardController.addObserver(ghostMoveObserver);
 
@@ -55,38 +43,23 @@ public class Main {
 
         CommonMaze commonMaze = cfg.createMaze();
 
-        EventsSystem.getInstance().on(EVENTS.KEYDOWN, (Object codes) -> {
-            Integer code = (Integer) ((Object[]) codes)[0];
-            PacmanObject pacman = commonMaze.getPacman();
+        GameController controller = new GameController(commonMaze, GAME_MODE.CONTINUOUS);
+        controller.start();
 
-            switch (code) {
-                case KeyEvent.VK_W -> pacman.move(CommonField.Direction.U);
-                case KeyEvent.VK_S -> pacman.move(CommonField.Direction.D);
-                case KeyEvent.VK_A -> pacman.move(CommonField.Direction.L);
-                case KeyEvent.VK_D -> pacman.move(CommonField.Direction.R);
-            }
-
-
-            return null;
-        });
-
-        MazePresenter presenter = new MazePresenter(commonMaze);
-        presenter.open();
-
-
-//        List<CommonMazeObject> ghosts = commonMaze.ghosts();
-//        CommonMazeObject ghost = null;
-//        for (CommonMazeObject _ghost : ghosts) {
-//            if (_ghost != null) {
-//                ghost = _ghost;
-//                break;
+//        GhostObject firstGhost = null;
+//        for (CommonField[] fields : commonMaze.getFields()) {
+//            for (CommonField field : fields) {
+//                if (field.get() instanceof GhostObject) {
+//                    firstGhost = (GhostObject) field.get();
+//                }
 //            }
 //        }
-
-//        ghost.move(CommonField.Direction.R);
+//
 //        sleep(1000);
-//        ghost.move(CommonField.Direction.R);
-//        sleep(1000);
+//
+//        if (firstGhost != null) {
+//            firstGhost.move(CommonField.Direction.U);
+//        }
     }
 
     public static void sleep(int ms) {
