5a77c9fc841ef599cfd948f2b84b31b0736b9181
rediso4ka <swimmer030203@gmail.com>
2023-05-07 10:46:44 +0200

    feat: added key image and view

---
+++ src/main/java/ija/ija2022/project/objects/KeyView.java
@@ -0,0 +1,37 @@
+package ija.ija2022.project.objects;
+
+import ija.ija2022.project.common.ComponentView;
+import ija.ija2022.project.fields.FieldView;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class KeyView implements ComponentView {
+    private final FieldView parent;
+
+    public KeyView(FieldView parent, CommonMazeObject m) {
+        this.parent = parent;
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        BufferedImage keyImage = null;
+        try {
+            keyImage = ImageIO.read(getClass().getResource("/key.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Rectangle bounds = this.parent.getBounds();
+        double w = bounds.getWidth();
+        double h = bounds.getHeight();
+        double diameter = Math.min(h, w) - 10.0;
+        double x = (w - diameter) / 2.0;
+        double y = (h - diameter) / 2.0;
+
+        g2.drawImage(keyImage, (int) x, (int) y, (int) diameter, (int) diameter, null);
+    }
+}
